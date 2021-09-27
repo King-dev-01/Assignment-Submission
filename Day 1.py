aba41e4d6c7d55c8deaedec1c7a16b26da1f56e7
King-dev-01 <82051558+King-dev-01@users.noreply.github.com>
2021-09-27 21:51:15 +0530

    Assignment Day 1

---
+++ Day 1.py
@@ -0,0 +1,26 @@
+a = 20
+b = 10.5
+name = "King"
+value = True
+
+print(type(a))
+print(type(b))
+print(type(name))
+print(type(value))
+
+print(a+b)
+print(a-b)
+print(a*b)
+print(a/b)
+print(a//b)
+print(a**b)
+print(a%b)
+
+print(a<b)
+print(a>b)
+print(a<=b)
+print(a>=b)
+print(a==b)
+print(a!=b)
+
+print("Assignment Completed. \n Thank You!")
