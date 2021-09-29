9e83d0d1c72eea3ed339c1e539a52b1797c16d38
King-dev-01 <82051558+King-dev-01@users.noreply.github.com>
2021-09-29 23:48:28 +0530

    Day 1 and Day 2 Assignment

---
+++ rec.py
@@ -0,0 +1,68 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+x = 2
+y = 5
+xy = 'Hey'
+print(x+y,xy)
+
+
+# In[3]:
+
+
+print("""My name is Rajath Kumar M.P.
+I love Python.""")
+
+
+# In[4]:
+
+
+string1 = 'World'
+print('Hello',string1)
+
+string2 = '!'
+print('Hello',string1,string2)
+
+
+# In[5]:
+
+
+print('Hello'+ string1 + string2)
+
+
+# In[6]:
+
+
+print("Jan\nFeb\nMar\nApr\nMay\nJun\nJul\nAug")
+
+
+# In[8]:
+
+
+print("I want \\n to be printed.")
+
+
+# In[9]:
+
+
+x = 10
+print(type(x))
+
+s ='abc'
+print(type(s))
+
+value = True
+print(type(value))
+
+percent=90.00
+type(percent)
+
+
+# In[ ]:
+
+
+
+
